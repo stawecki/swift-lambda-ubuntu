b9d3597c2686eac1a28829efa134b746364557ba
Mateusz Stawecki <mateusz.stawecki@othermedia.com>
2018-09-17 16:27:35 +0100

    GameCharacter - added properties.

--- Sources/Shared/Game.swift
+++ Sources/Shared/Game.swift
@@ -1,21 +1,29 @@
+import Foundation
 
-public struct GameCharacter: Encodable {
-    let name: String
+public class GameCharacter: Encodable {
+    public init() {
+        name = "Unnamed"
+    }
+    var name: String
+    var intelligence: Int = 0
+    var charisma: Int = 0
+    var strength: Int = 0
+    var dexterity: Int = 0
 }
 
 public class Game {
-    public init() {
-
-    }
+    public init() {}
 
     public func generatePlayer(name: String?) -> GameCharacter {
-        let player = GameCharacter(name: (name != nil ? name! : "Unnamed") )
+        let player = GameCharacter()
+        if (name != nil) {
+            player.name = name!
+        }
+        player.intelligence = Int( arc4random_uniform(100) )
+        player.charisma = Int( arc4random_uniform(100) )
+        player.strength = Int( arc4random_uniform(100) )
+        player.dexterity = Int( arc4random_uniform(100) )
         return player
-    }
-
-    
-    public func test() -> String {
-      return "test"
     }
 }
 
