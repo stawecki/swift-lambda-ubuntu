0a913c0a651b7890ecfec00dff43a026804ae5df
Mateusz Stawecki <mateusz.stawecki@othermedia.com>
2018-09-17 15:15:55 +0100

    Updated Kitura

--- Package.swift
+++ Package.swift
@@ -1,18 +1,31 @@
+// swift-tools-version:4.0
 import PackageDescription
 
+#if os(macOS)
+// macOS: Kitura
 var package = Package(
     name: "Game",
-    targets: [
-        Target(name: "Shared"),
-        Target(name: "Lambda", dependencies: ["Shared"])
-    ],
     dependencies: [
-    ], 
-    exclude: ["Sources/Server"])
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.5.0")),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1"))
+    ],
+    targets: [
+      .target(name: "Shared"),
+      .target(name: "Server", dependencies: ["Shared","Kitura","HeliumLogger"])
+    ]
+)
 
-#if os(macOS)
-package.targets.append(Target(name: "Server", dependencies: ["Shared"]))
-package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 1))
-package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 1))
-package.exclude = []
+#else
+// Ubuntu: Lambda build
+var package = Package(
+    name: "Game",
+    exclude: ["Sources/Server"],
+    dependencies: [
+    ],
+    targets: [
+      .target(name: "Shared"),
+      .target(name: "Lambda", dependencies: ["Shared"])
+    ]
+)
+
 #endif
--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -1,12 +1,22 @@
 import Foundation
 import Kitura
 import HeliumLogger
+import Shared
 
 let router = Router()
 router.get("/ping") { request, response, next in
     response.send("pong")
     next()
 }
+
+router.get("/test") { request, response, next in
+    let game = Game();
+    let gameTest = game.test();
+    response.send(gameTest)
+    next()
+}
+
+print("Running: http://localhost:8090/test")
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
